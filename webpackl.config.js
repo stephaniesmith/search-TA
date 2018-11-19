fd4192e397624929621179899dd8c9ba6d688769
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-11-19 10:55:17 -0800

    webpack and babel

---
+++ webpackl.config.js
@@ -0,0 +1,67 @@
+/* eslint-env node */
+const HtmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.[hash].js',
+    publicPath: ''
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 7899,
+  },
+  plugins: [
+    new HtmlPlugin({ template: './src/index.html' })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: { sourceMap: true }
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              modules: true,
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              plugins: [
+                require('autoprefixer')(),
+                require('postcss-nested')(),
+                require('postcss-simple-vars')()
+              ]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: { limit: 1000 },
+        },
+      }
+    ]
+  }
+};
